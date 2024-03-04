1e702c156aacb54f1a16a57c44137157b1f8fda6
Jiang Ruiying <ruiying.23@intl.zju.edu.cn>
2024-03-04 19:28:54 +0800

    edit variables

---
+++ Practical4/recursive.py
@@ -0,0 +1,10 @@
+# give values to the initial values m, n, and q
+m=4
+n=11
+i=1
+print(m)
+for i in range (1,8):
+    n=2*m
+    n=n+3
+    m=n
+    print(n)
---
+++ Practical4/variables.py
@@ -0,0 +1,11 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+print('d=',d)
+print('e=',e)
+if d<e:
+    print('e is greater.')
+# e is greater than d. 
+# Using a combination of running and strength exercises to train the athletes is more effective.
